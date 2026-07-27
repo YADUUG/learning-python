44e50911f410545c602521a45f16ced4b37f7a2e
Gurwinder Singh <gurwindersingh7769@gmail.com>
2026-07-27 14:53:54 +0530

    class hos

--- gur.py
+++ gur.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+# def add(a,b):
+#     s = a+b
+#     print(s)
+# add(5, 10)    
+
+# def add(a,b):
+#     s = a+b
+#     return s
+# result = add(5,10)
+# print(result)
+
+class hos:
+    location = "Amritsar"
+
+    def __init__(self, name,age,disease):
+        self.name = name
+        self.age = age
+        self.disease = disease
+
+patient1 = hos("modi", 70,"fever")
+patient2 = hos("rohan", 50,"flu")
+
+print("Patient1:",patient1.name)
+print("patient1 age:",patient1.age)
+print("patient1 disease:",patient1.disease)
+print("patient1 location:",patient1.location)
