47fabe8fc6efc358f05052b876f854fb546b71ee
c5ffjftjfv-arch <jagdish3003@icloud.com>
2026-07-28 14:52:58 +0530

    calculator

--- jag.py
+++ jag.py
@@ -1,11 +1,12 @@
-def ab(a,b,c):
-    return a + b + c
-
-print(ab(1,2,3))
-
-###########
-
-def ab(a,b,c):
-    sum = a + b + c
-
-print(ab(1,2,3))
+give_command= int(input("enter the command: 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division: "))
+def calc(a,b):
+    if give_command == 1:
+        return a+b
+    elif give_command == 2:
+        return a-b
+    elif give_command == 3:
+        return a*b
+    else:
+        return a/b
+    
+print(calc(8,6))
