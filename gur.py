e1c9e92737d2199e0e9678fdeb3d320468e040dc
Gurwinder Singh <gurwindersingh7769@gmail.com>
2026-07-28 14:48:55 +0530

    clasess & objects

--- gur.py
+++ gur.py
@@ -11,18 +11,27 @@
 # result = add(5,10)
 # print(result)
 
-class hos:
-    location = "Amritsar"
+# class hos:
+#     location = "Amritsar"
 
-    def __init__(self, name,age,disease):
-        self.name = name
-        self.age = age
-        self.disease = disease
+#     def __init__(self, name,age,disease):
+#         self.name = name
+#         self.age = age
+#         self.disease = disease
 
-patient1 = hos("modi", 70,"fever")
-patient2 = hos("rohan", 50,"flu")
+# patient1 = hos("modi", 70,"fever")
+# patient2 = hos("rohan", 50,"flu")
 
-print("Patient1:",patient1.name)
-print("patient1 age:",patient1.age)
-print("patient1 disease:",patient1.disease)
-print("patient1 location:",patient1.location)
+# print("Patient1:",patient1.name)
+# print("patient1 age:",patient1.age)
+# print("patient1 disease:",patient1.disease)
+# print("patient1 location:",patient1.location)
+
+def func(x):
+    if x ==0:
+        return True
+
+    print(func(0))
+    return False
+
+print(func(10))
